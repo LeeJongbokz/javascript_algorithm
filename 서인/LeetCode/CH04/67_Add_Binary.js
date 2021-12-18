e3f926312e68fee680e84eaa09dc734634fcbbd6
inseo24 <84627144+inseo24@users.noreply.github.com>
2021-12-18 21:48:54 +0900

    add : leetcode CH04 #54(unsolved), #67

---
+++ 서인/LeetCode/CH04/67_Add_Binary.js
@@ -0,0 +1,12 @@
+// 67. Add Binary
+// https://leetcode.com/problems/add-binary/
+
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+ var addBinary = function(a, b) {
+    
+    return (BigInt(`0b${a}`) + BigInt(`0b${b}`)).toString(2);  
+};

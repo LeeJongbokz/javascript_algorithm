ca8aafdefe19c498bba02c5960705776130e05de
LeeJongbok <s2feeling@naver.com>
2021-12-31 15:45:16 +0900

    Create 2108(sub). Find First Palindromic String in the Array.js

---
+++ 종복/효율성2주차/2108(sub). Find First Palindromic String in the Array.js
@@ -0,0 +1,11 @@
+var firstPalindrome = function(words) {
+    
+    for(let i=0; i<words.length; i++){
+        if(words[i] === words[i].split("").reverse().join("")){
+            return words[i];
+        }
+    }
+    
+    return "";
+    
+};

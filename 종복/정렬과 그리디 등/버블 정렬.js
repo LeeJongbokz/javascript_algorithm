82dfe0ac8df4d642d3818be6d871784619427c5d
LeeJongbok <s2feeling@naver.com>
2022-01-16 00:54:45 +0900

    Create 버블 정렬.js

---
+++ 종복/정렬과 그리디 등/버블 정렬.js
@@ -0,0 +1,24 @@
+function solution(arr){
+    let answer=arr;
+
+    for(let i=0; i<arr.length; i++){
+        let swap;
+
+        for(let j=0; j<arr.length-1-i; j++){
+            if(arr[j] > arr[j+1]){
+                swap = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = swap;
+            }
+        }
+
+        if(!swap){
+            break;
+        }
+    }
+
+    return answer; 
+}
+
+let arr=[13, 5, 11, 7, 23, 15];
+console.log(solution(arr));

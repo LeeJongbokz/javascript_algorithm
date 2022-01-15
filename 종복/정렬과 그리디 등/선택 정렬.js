b6f01ef1770515f08d35d5ce64273e90449efe61
LeeJongbok <s2feeling@naver.com>
2022-01-16 00:32:29 +0900

    Create 선택 정렬.js

---
+++ 종복/정렬과 그리디 등/선택 정렬.js
@@ -0,0 +1,27 @@
+function solution(arr){
+
+    let answer = arr;
+
+    for(let i=0; i<arr.length; i++){
+
+        let minIndex = i;
+
+        for(let j=i+1; j<arr.length; j++){
+            if(arr[minIndex] > arr[j]){
+                minIndex = j;
+            }
+        }
+
+        if(minIndex !== i){
+            let swap = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = swap;
+        }
+    }
+
+    return answer;     
+
+}
+
+let arr=[13, 5, 11, 7, 23, 15];
+console.log(solution(arr));

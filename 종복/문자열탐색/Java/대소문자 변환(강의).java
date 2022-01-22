5d5b59549642a166496344a9cd7eb335c65deeb2
LeeJongbok <s2feeling@naver.com>
2022-01-22 18:25:07 +0900

    Create 대소문자 변환(강의).java

---
+++ 종복/문자열탐색/Java/대소문자 변환(강의).java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+  
+public class Main {
+  
+  
+  public String solution(String str){
+    
+    String answer = ""; 
+   
+    for(char x : str.toCharArray()){
+       
+      if(Character.isLowerCase(x)) answer += Character.toUpperCase(x);
+      else answer += Character.toLowerCase(x);  
+    }
+    
+    return answer; 
+    
+  }
+  
+  
+  public static void main(String[] args){
+    
+    Main T = new Main();
+    Scanner kb=new Scanner(System.in);
+    String str = kb.next();
+    
+    System.out.println(T.solution(str));
+    
+  }
+}

c39d1c3857c55fb82d404489b10cf28a749b6608
LeeJongbok <s2feeling@naver.com>
2022-01-26 14:55:58 +0900

    Create 올바른 괄호(3).js

---
+++ 종복/스택&큐/올바른 괄호(3).js
@@ -0,0 +1,26 @@
+function solution(s){
+    let answer="YES";
+    let stack = [];
+
+
+    for(let x of s){
+        if(x === '('){
+            stack.push(x);
+        }else{
+            if(stack.length === 0){
+                return "NO";
+            }else{
+                stack.pop();
+            }
+        }
+    }
+
+    if(stack.length !== 0){
+        return "NO"
+    }
+
+    return answer;
+}
+
+let a="(()(()))(()";
+console.log(solution(a));

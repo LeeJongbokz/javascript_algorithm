3c9cc88234bf5baf0684fa6d162e132b87732d4c
LeeJongbok <s2feeling@naver.com>
2022-03-02 16:30:07 +0900

    Create 5. 소수.java

---
+++ 종복/Java/Array/5. 소수.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Main {	
+ 
+   	public int solution(int n){
+		    int cnt=0;
+      	int[] ch = new int[n+1];
+      
+        for(int i=2; i<=n; i++){
+            
+            if(ch[i] == 0){
+               cnt += 1;
+              
+               for(int j=i; j<=n; j+=i){
+                   ch[j] = 1; 
+               }
+            }
+        }   
+        
+      
+		return cnt;
+	}
+	public static void main(String[] args){
+		Main T = new Main();
+		Scanner kb = new Scanner(System.in);
+		int n=kb.nextInt();
+		System.out.println(T.solution(n));
+	}
+}

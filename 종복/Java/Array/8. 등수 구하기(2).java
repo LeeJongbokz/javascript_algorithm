2689a2014a2375fed414be599d3f3b4ef20466bd
LeeJongbok <s2feeling@naver.com>
2022-03-04 16:29:32 +0900

    Create 8. 등수 구하기(2).java

---
+++ 종복/Java/Array/8. 등수 구하기(2).java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Main {	
+	public int[] solution(int n, int[] arr){
+        int[] answer = new int[n];
+      
+        for(int i=0; i<n; i++){
+          int cnt = 1;
+          
+          for(int j=0; j<n; j++){
+            if(arr[j] > arr[i]) cnt++;
+          }
+          answer[i] = cnt;
+        }
+      
+        return answer;
+		
+	}
+	public static void main(String[] args){
+		Main T = new Main();
+		Scanner kb = new Scanner(System.in);
+		int n=kb.nextInt();
+		int[] arr=new int[n];
+		for(int i=0; i<n; i++){
+			arr[i]=kb.nextInt();
+		}
+		for(int x :T.solution(n, arr)) System.out.print(x+" ");
+	}
+}

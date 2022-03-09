e976587d57dc6a08b3aab4651143490dbc457639
hunmaker <hunhoohunhoo1@gmail.com>
2022-03-09 17:42:53 +0900

    프로그래머스 주식가격

---
+++ 영훈/Programmers/스택, 큐/주식가격.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> prices) {
+    vector<int> answer;
+    
+    for (int i = 0; i < prices.size() - 1; i++) {
+        for (int j = i + 1; j < prices.size(); j++) {
+            if (prices[i] > prices[j]) {
+                answer.push_back(j - i);
+                break;
+            }
+            if (j == prices.size() - 1) {
+                answer.push_back(prices.size() - i - 1);
+            }
+        }
+    }
+    answer.push_back(0);
+    
+    return answer;
+}

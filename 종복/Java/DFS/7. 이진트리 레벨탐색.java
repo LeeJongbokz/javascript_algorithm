cced2e4941ccb8d3356a6b7aa761a03dc422d088
LeeJongbok <s2feeling@naver.com>
2022-04-03 01:25:44 +0900

    Update 7. 이진트리 레벨탐색.java

--- 종복/Java/DFS/7. 이진트리 레벨탐색.java
+++ 종복/Java/DFS/7. 이진트리 레벨탐색.java
@@ -39,7 +39,7 @@ public class Main{
         tree.root.rt=new Node(3); 
         tree.root.lt.lt=new Node(4); 
         tree.root.lt.rt=new Node(5); 
-		    tree.root.rt.lt=new Node(6); 
+        tree.root.rt.lt=new Node(6); 
         tree.root.rt.rt=new Node(7);
         tree.BFS(tree.root); 
     } 

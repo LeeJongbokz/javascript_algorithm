f7b6543bb254aaf925b7662c4234967c65dedf88
LeeJongbok <s2feeling@naver.com>
2022-04-06 02:30:14 +0900

    Create 9. Tree 말단 노드까지의 가장 짧은 경로(BFS)(2).java

---
+++ 종복/Java/DFS/9. Tree 말단 노드까지의 가장 짧은 경로(BFS)(2).java
@@ -0,0 +1,46 @@
+import java.util.*;
+class Node{ 
+    int data; 
+    Node lt, rt; 
+    public Node(int val) { 
+        data=val; 
+        lt=rt=null; 
+    } 
+} 
+  
+public class Main{ 
+    Node root; 
+	
+	
+	public int BFS(Node root){
+	
+    	Queue<Node> q = new LinkedList<>();
+    	q.offer(root);
+    	int L = 0; 
+    	
+    	while(!q.isEmpty()){
+    	    int len = q.size();
+    	    
+    	    for(int i=0; i<len; i++){
+    	        Node cur = q.poll();
+    	        if(cur.lt == null && cur.rt == null) return L;
+    	        if(cur.lt != null) q.offer(cur.lt);
+    	        if(cur.rt != null) q.offer(cur.rt);
+    	    }
+    	    L++;
+    	}
+	    
+	    return 0;
+	}
+	
+	
+    public static void main(String args[]) { 
+        Main tree=new Main(); 
+        tree.root=new Node(1); 
+        tree.root.lt=new Node(2); 
+        tree.root.rt=new Node(3); 
+        tree.root.lt.lt=new Node(4); 
+        tree.root.lt.rt=new Node(5); 
+        System.out.println(tree.BFS(tree.root)); 
+    } 
+}

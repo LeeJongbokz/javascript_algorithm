f127e42fd193a3646b4999d0d44007406410def0
LeeJongbok <s2feeling@naver.com>
2022-04-09 00:22:31 +0900

    Create 3. 최대점수 구하기.java

---
+++ 종복/Java/DFS 활용/3. 최대점수 구하기.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main{
+	static int answer=Integer.MIN_VALUE, n, m;
+	boolean flag=false;
+	public void DFS(int L, int sum, int time, int[] ps, int[] pt){
+	  
+	   if(L == n){
+	       if(time<=m){
+	           answer = Math.max(answer, sum);
+	       }
+	       return; 
+	   }else{
+	       DFS(L+1, sum+ps[L], time+pt[L], ps, pt);
+	       DFS(L+1, sum, time, ps, pt);
+	   }
+	
+	}
+
+	public static void main(String[] args){
+		Main T = new Main();
+		Scanner kb = new Scanner(System.in);
+		n=kb.nextInt();
+		m=kb.nextInt();
+		int[] a=new int[n];
+		int[] b=new int[n];
+		for(int i=0; i<n; i++){
+			a[i]=kb.nextInt();
+			b[i]=kb.nextInt();
+		}
+		T.DFS(0, 0, 0, a, b);
+		System.out.println(answer);
+	}
+}

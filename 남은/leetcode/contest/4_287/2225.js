c6fdd31f38d4e6957d9fa81304085c90a0e2cc46
1 <a1@1ui-MacBookPro.local>
2022-04-09 19:08:28 +0900

    ADD: 2225 of 4th contest

--- 남은/leetcode/contest/4_287/2225.js
+++ 남은/leetcode/contest/4_287/2225.js
@@ -10,18 +10,19 @@
  * @return {number[][]}
  */
 
-// const matches = [
-//   [1, 3],
-//   [2, 3],
-//   [3, 6],
-//   [5, 6],
-//   [5, 7],
-//   [4, 5],
-//   [4, 8],
-//   [4, 9],
-//   [10, 4],
-//   [10, 9],
-// ];
+const matches = [
+  [1, 3],
+  [2, 3],
+  [3, 6],
+  [5, 6],
+  [5, 7],
+  [4, 5],
+  [4, 8],
+  [4, 9],
+  [10, 4],
+  [10, 9],
+];
+
 // const matches = [
 //   [1, 5],
 //   [2, 5],
@@ -40,61 +41,56 @@
 //   [5, 4],
 //   [6, 4],
 // ];
-const matches = [
-  [1, 5],
-  [1, 6],
-  [2, 5],
-  [2, 7],
-  [2, 8],
-  [3, 4],
-  [3, 9],
-  [4, 6],
-  [4, 9],
-  [5, 6],
-  [5, 8],
-  [6, 7],
-  [6, 8],
-];
+// const matches = [
+//   [1, 5],
+//   [1, 6],
+//   [2, 5],
+//   [2, 7],
+//   [2, 8],
+//   [3, 4],
+//   [3, 9],
+//   [4, 6],
+//   [4, 9],
+//   [5, 6],
+//   [5, 8],
+//   [6, 7],
+//   [6, 8],
+// ];
 var findWinners = function (matches) {
   const answer = [[], []];
-
-  const winners = [];
+  const winners = {};
   const losers = {};
-  //   const losers = [];
+  const uniquePlayers = new Set();
 
   matches.forEach((elem) => {
-    if (!losers[elem[1]]) {
-      losers[elem[1]] = 1;
-      answer[1].push(elem[1]);
-    } else if (losers[elem[1]]) {
-      losers[elem[1]] = losers[elem[1]] + 1;
-      const idx = answer[1].indexOf(elem[1]);
-      if (idx !== -1) answer[1].splice(idx, 1);
+    const [win, lose] = elem;
+    uniquePlayers.add(win);
+    uniquePlayers.add(lose);
+
+    if (winners[win]) {
+      winners[win] += 1;
+    } else {
+      winners[win] = 1;
     }
 
-    if (
-      !winners.includes(elem[0]) &&
-      !Object.keys(losers).includes(elem[0] + "")
-    )
-      winners.push(elem[0]);
-
-    if (winners.includes(elem[1])) {
-      const idx = winners.indexOf(elem[1]);
-      winners.splice(idx, 1);
+    if (losers[lose]) {
+      losers[lose] += 1;
+    } else {
+      losers[lose] = 1;
     }
   });
-  //   console.log({ winners });
-  //   console.log({ losers });
-  answer[0] = winners.sort(function (a, b) {
+
+  // console.log({ winners });
+
+  const players = [...uniquePlayers];
+  players.sort(function (a, b) {
     return a - b;
   });
-  answer[1].sort(function (a, b) {
-    return a - b;
+  // console.log({ players });
+  players.forEach((elem) => {
+    if (!losers[elem] && winners[elem]) answer[0].push(elem);
+    if (losers[elem] === 1) answer[1].push(elem);
   });
-  // 2. loswer에 1번만 들어가는 애 카운트하기
-  //   for (const property in losers) {
-  //     if (losers[property] === 1) answer[1].push(property * 1);
-  //   }
 
   return answer;
 };

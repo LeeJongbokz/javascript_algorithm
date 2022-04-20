7dda7609dbdb0965e1d8a63e29d1e93279cb0738
1 <a1@1ui-MacBookPro.local>
2022-04-20 16:57:17 +0900

    ADD: 234

---
+++ 남은/leetcode/스터디/599.js
@@ -0,0 +1,7 @@
+/**
+ * @param {string[]} list1
+ * @param {string[]} list2
+ * @return {string[]}
+ */
+var findRestaurant = function (list1, list2) {};
+console.log(findRestaurant(list1, list2));
---
+++ 남은/leetcode/스터디/83.js
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function (head) {};
